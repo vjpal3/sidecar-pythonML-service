c5c8b3b0166e4237a2d024ea42e25319801f5473
vjpal3 <vrishalipal@yahoo.com>
2020-05-24 21:41:22 -0400

    Setup sidecar to call python microservice

---
+++ src/main/java/com/vrishalipal/microservices/sidecarpythonservice/SidecarPythonServiceApplication.java
@@ -0,0 +1,15 @@
+package com.vrishalipal.microservices.sidecarpythonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@EnableSidecar
+@SpringBootApplication
+public class SidecarPythonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarPythonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrishalipal/microservices/sidecarpythonservice/SidecarPythonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrishalipal.microservices.sidecarpythonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidecarPythonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
